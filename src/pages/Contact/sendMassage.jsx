da2fdda96ac46f28ed149619ce6c83f36bae7aad
Mahmudbek <mahmudbekkjumayev@gmail.com>
2024-11-24 23:24:38 +0500

    Send massage error

--- src/pages/Contact/sendMassage.jsx
+++ src/pages/Contact/sendMassage.jsx
@@ -1,43 +1,43 @@
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
+// export default async function handler(req, res) {
+//   if (req.method !== "POST") {
+//     return res.status(405).json({ error: "Method not allowed" });
+//   }
 
-  const { name, email, message } = req.body;
+//   const { name, email, message } = req.body;
 
-  if (!name || !email || !message) {
-    return res.status(400).json({ error: "All fields are required" });
-  }
+//   if (!name || !email || !message) {
+//     return res.status(400).json({ error: "All fields are required" });
+//   }
 
-  const telegramToken = "7385022476:AAEuDbLPJBCrHCQzuXDwQ-MzCb17ECk9-xE";
-  const chatId = "7385022476";
+//   const telegramToken = "7385022476:AAEuDbLPJBCrHCQzuXDwQ-MzCb17ECk9-xE";
+//   const chatId = "7385022476";
 
-  const text = `
-  New Contact Form Submission:
-  - Name: ${name}
-  - Email: ${email}
-  - Message: ${message}
-    `;
+//   const text = `
+//   New Contact Form Submission:
+//   - Name: ${name}
+//   - Email: ${email}
+//   - Message: ${message}
+//     `;
 
-  try {
-    const response = await fetch(
-      `https://api.telegram.org/bot${telegramToken}/sendMessage`,
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          chat_id: chatId,
-          text: text,
-        }),
-      }
-    );
+//   try {
+//     const response = await fetch(
+//       `https://api.telegram.org/bot${telegramToken}/sendMessage`,
+//       {
+//         method: "POST",
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify({
+//           chat_id: chatId,
+//           text: text,
+//         }),
+//       }
+//     );
 
-    if (!response.ok) {
-      throw new Error("Failed to send message");
-    }
+//     if (!response.ok) {
+//       throw new Error("Failed to send message");
+//     }
 
-    return res.status(200).json({ success: true, message: "Message sent" });
-  } catch (error) {
-    return res.status(500).json({ error: "Failed to send message" });
-  }
-}
+//     return res.status(200).json({ success: true, message: "Message sent" });
+//   } catch (error) {
+//     return res.status(500).json({ error: "Failed to send message" });
+//   }
+// }
