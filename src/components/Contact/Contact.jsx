985038364f6810c13d80a020220ae0b2a43f66d9
hojiakbar2806 <hojiakbarnasriddinov2006@gmail.com>
2024-11-30 23:03:40 +0500

    fixed adaptive

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 
-
 const Contact = () => {
   const [formData, setFormData] = useState({
     name: "",
@@ -39,66 +38,55 @@ const Contact = () => {
   };
 
   return (
-    <div id="contact" className="container mx-auto py-16 lg:py-24">
+    <div id="contact" className="container mx-auto py-16">
       <h4 className="text-4xl text-violet-600 font-bold text-left mb-10">
         Contact Us
       </h4>
-      <div className="shadow-xl bg-gray-100 p-10 flex flex-col md:flex-row justify-between items-center">
-        <div className="w-full md:w-1/2">
-          <form
-            onSubmit={handleSubmit}
-            className="flex flex-col gap-5 py-5 px-4 md:px-10"
+      <div className="shadow-xl bg-gray-100 gap-10 p-10 flex flex-col-reverse xl:flex-row justify-between">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-1 flex-col gap-5 px-4"
+        >
+          <input
+            className="p-3 rounded-md border-2 border-gray-300"
+            placeholder="Name"
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+          <input
+            className="p-3 rounded-md border-2 border-gray-300"
+            placeholder="Your email"
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+          <textarea
+            className="p-3 rounded-md border-2 border-gray-300"
+            placeholder="Briefly describe your project"
+            rows="5"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          ></textarea>
+          <button
+            type="submit"
+            className="bg-violet-600 w-full border-none hover:bg-violet-900 px-8 py-2 rounded-md text-white text-lg font-bold"
           >
-            <p className="text-[16px] md:text-[18px] text-gray-800 font-medium">
-              Leave us a message
-            </p>
-            <input
-              className="p-3 rounded-md border-2 border-gray-300"
-              placeholder="Name"
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              required
-            />
-            <input
-              className="p-3 rounded-md border-2 border-gray-300"
-              placeholder="Your email"
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-            <textarea
-              className="p-3 rounded-md border-2 border-gray-300"
-              placeholder="Briefly describe your project"
-              rows="5"
-              name="message"
-              value={formData.message}
-              onChange={handleChange}
-              required
-            ></textarea>
-            <button
-              type="submit"
-              className="bg-violet-600 w-full md:w-[200px] border-none hover:bg-violet-900 px-8 py-2 rounded-md text-white text-lg font-bold"
-            >
-              Send
-            </button>
-            {status && <p className="text-gray-700 mt-2">{status}</p>}
-          </form>
-        </div>
-        <div className="w-full md:w-1/2 flex flex-col justify-start items-start p-5 md:p-10 gap-5">
-          <div style={{ position: "relative", overflow: "hidden" }}>
-            <iframe
-              src="https://yandex.uz/map-widget/v1/?l=night&ll=69.216012%2C41.234714&mode=search&oid=197913283981&ol=biz&rl=69.214078%2C41.234649&sctx=ZAAAAAgBEAAaKAoSCTyHMlTFvVlAEbudfeVBnkRAEhIJAAAAAICNb0ARUaT7OQXQWUAiBgABAgMEBSgKOABAkE5IAWoCdXqdAc3MzD2gAQCoAQC9AYTeFOvCAQaNm7Sk4QWCAhBQZHAgVW5pdmVyc2l0ZXRpigIAkgIAmgIMZGVza3RvcC1tYXBz&sll=69.216012%2C41.234714&sspn=0.044802%2C0.019713&text=Pdp%20Universiteti&z=15"
-              width="560"
-              height="400"
-              allowFullScreen={true}
-              style={{ position: "relative" }}
-            />
-          </div>
-        </div>
+            Send
+          </button>
+          {status && <p className="text-gray-700 mt-2">{status}</p>}
+        </form>
+        <iframe
+          className="flex-1 min-h-60"
+          src="https://yandex.uz/map-widget/v1/?l=night&ll=69.216012%2C41.234714&mode=search&oid=197913283981&ol=biz&rl=69.214078%2C41.234649&sctx=ZAAAAAgBEAAaKAoSCTyHMlTFvVlAEbudfeVBnkRAEhIJAAAAAICNb0ARUaT7OQXQWUAiBgABAgMEBSgKOABAkE5IAWoCdXqdAc3MzD2gAQCoAQC9AYTeFOvCAQaNm7Sk4QWCAhBQZHAgVW5pdmVyc2l0ZXRpigIAkgIAmgIMZGVza3RvcC1tYXBz&sll=69.216012%2C41.234714&sspn=0.044802%2C0.019713&text=Pdp%20Universiteti&z=15"
+          allowFullScreen={true}
+        />
       </div>
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -22,9 +22,7 @@ const Navbar = () => {
         </div>
         <div className="flex justify-between items-center gap-10">
           <ul
-            className={`lg:flex gap-2 ${
-              menuOpen ? "flex flex-col" : "hidden"
-            } lg:flex-row lg:items-center text-[14px] md:text-[16px] font-medium list-none p-0 m-0 cursor-pointer`}
+            className={`lg:flex hidden gap-2 lg:flex-row lg:items-center text-[14px] md:text-[16px] font-medium list-none p-0 m-0 cursor-pointer`}
           >
             {[
               { name: "Home", href: "#header" },
@@ -45,7 +43,12 @@ const Navbar = () => {
             <li className="relative group p-4">
               <span className="flex justify-center items-center gap-2">
                 Language
-                <Image width={20} height={20} src="/assets/icons/pastga.svg" alt="icon" />
+                <Image
+                  width={20}
+                  height={20}
+                  src="/assets/icons/pastga.svg"
+                  alt="icon"
+                />
               </span>
               <div className="absolute p-2 bg-white hidden group-hover:block text-black mt-1 rounded-md shadow-lg">
                 {[
@@ -69,40 +72,53 @@ const Navbar = () => {
               </div>
             </li>
           </ul>
-          <div className="hidden lg:block ">
-            <a href="#contact">
-              <button
-                type="button"
-                className="bg-violet-600 border-none hover:bg-violet-900 px-4 py-2 rounded-md text-white text-[20px] font-bold"
-              >
-                Contact
-              </button>
-            </a>
-          </div>
+
           <div className="lg:hidden cursor-pointer" onClick={toggleMenu}>
             <Image
-              className={`w-[30px] h-[30px] ${menuOpen ? "hidden" : "block"}`}
+              className={`w-[30px] h-[30px]`}
               src="/assets/images/burgermenu.png"
               alt="burger menu"
-              width={30}
-              height={30}
-            />
-            <Image
-              className={`w-[30px] h-[30px] ${menuOpen ? "block" : "hidden"}`}
-              src="/assets/images/close.png"
-              alt="close menu"
               width={30}
               height={30}
             />
           </div>
         </div>
       </div>
+
+      <div
+        className={`absolute top-0 left-0 w-full transition-all duration-300 h-screen bg-black/25 ${
+          menuOpen
+            ? "opacity-100 pointer-events-auto"
+            : "opacity-0 pointer-events-none"
+        }`}
+        onClick={closeMenu}
+      >
+        <div
+          className={`absolute top-0 right-0 w-[300px] h-full bg-white transform transition-transform duration-300 ${
+            menuOpen ? "translate-x-0" : "translate-x-full"
+          }`}
+        >
+          <ul className="p-4">
+            {[
+              { name: "Home", href: "#header" },
+              { name: "Service", href: "#service" },
+              { name: "Consulting", href: "#consulting" },
+              { name: "Work", href: "#work" },
+            ].map((item) => (
+              <li
+                key={item.href}
+                onClick={closeMenu}
+                className="relative group p-4 cursor-pointer"
+              >
+                <a href={item.href}>{item.name}</a>
+                <span className="absolute bottom-0 left-0 w-full h-0.5 bg-transparent group-hover:bg-violet-600 transition-all duration-300"></span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </nav>
   );
-};
-
-const MainContent = () => {
-  return <div className="pt-[80px]"></div>;
 };
 
 const Page = () => {
@@ -114,7 +130,6 @@ const Page = () => {
         }
       `}</style>
       <Navbar />
-      <MainContent />
     </>
   );
 };
